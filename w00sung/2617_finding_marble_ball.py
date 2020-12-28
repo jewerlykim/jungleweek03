d697153b66db0039ee3ddb42fef76186214ef0db
w00sung <jeongws3240@gmail.com>
2020-12-28 23:48:37 +0900

    DFS & BFS 12/28

--- w00sung/2617_finding_marble_ball.py
+++ w00sung/2617_finding_marble_ball.py
@@ -6,8 +6,11 @@ n_marble, n_comp = map(int,read().rstrip().split())
 graph = [[] for _ in range(n_marble + 1)]
 visited = [False] * (n_marble + 1)
 ans_li = [[0,0] for _ in range(n_marble+1)]
+# 연결 안된 녀석들, dfs돌릴 녀석들
+first = [True] *  (n_marble + 1)
 # small_cnt = [0] * (n_marble + 1)
 # big_cnt = [0] * (n_marble + 1)
+# ans = []
 
 for _ in range(n_comp):
 
@@ -15,6 +18,8 @@ for _ in range(n_comp):
     
     # 나보다 무거운 녀석들을 담는다.
     graph[small].append(big)
+    # 가장 작은 녀석들만 dfs 돌릴거임
+    first[big] = False
 
 # 얼마나 더 들어갈 수 있는지 or 얼마나 더 들어왔는지
 
@@ -26,13 +31,11 @@ def count_ball_dfs(marble, depth,mid):
     # small_cnt[marble] += (depth-1)
     
     # 들어왔는데 true?
-    # if visited[marble] == True:
-    #     ans_li[marble]
+    if visited[marble] == True:
+        ans_li[marble]
 
+    # 가벼운 녀석 개수
     ans_li[marble][1] += (depth-1)
-
-
-    visited[marble] = True
 
     for bigger in graph[marble]:
         # 찔린 것도 찔릴 수 있게, 들어갈 때 visited 조건 빼준다.
@@ -40,22 +43,29 @@ def count_ball_dfs(marble, depth,mid):
 
         # 들어간 깊이를 받아와서 계속 쌓아줌
         num = count_ball_dfs(bigger,depth + 1,mid)
+        
+        # 나보다 큰 녀석의 무거운 개수 얹기
         cnt += num 
-        cnt += 1
+        # DFS 들어가서 나올 때, + 1
+        cnt += 1 
 
     # 나보다 큰 녀석들 개수 -- depth 들어간 개수
     # big_cnt[marble] = cnt
     # 무거운 녀석이 중앙값 개수보다 크면 출력
     # if cnt >= mid:
     #     ans_li.append(marble)
+    
+    # 무거운 녀석 수
     ans_li[marble][0] = cnt
+
     return cnt
 
 mid =(1+n_marble) // 2
 
 # 반드시 1부터 시작하라는 법 없음..
 for i in range(1,(n_marble+1)):
-    if visited[i] == False:
+    # 얘네 보다 작은 것 없는 녀석들만 dfs돌린다.
+    if first[i] == True:
         count_ball_dfs(i,1,mid)
 
 ans = 0
